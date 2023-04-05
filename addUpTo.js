5c8446a94073aa07d1b1329ae7d60fa940745727
seunghyun <cvtcvt007@naver.com>
2023-04-05 15:47:14 +0900

    section4

--- addUpTo.js
+++ addUpTo.js
@@ -7,7 +7,7 @@ function addUpTo2(n) {
   for (let i = 1; i <= n; i++) result += i;
   console.log(result);
 }
-
+``;
 function countUpAndDown(n) {
   console.log("Going up!");
   for (var i = 0; i < n; i++) {
@@ -19,11 +19,20 @@ function countUpAndDown(n) {
   }
   console.log("Back down. Bye!");
 }
-
+function logAllBinaries(n) {
+  var count = 0;
+  var lastNum = "1".repeat(n);
+  while (count.toString(2) !== lastNum) {
+    console.log(count.toString(2).padStart(n, "0"));
+    count++;
+  }
+  console.log(lastNum);
+}
 let t1 = performance.now();
 //let result = addUpTo1(1000000000);
 //let result = addUpTo2(1000000000);
-let result = countUpAndDown(10000);
+//let result = countUpAndDown(10000);
+let result = logAllBinaries(10);
 let t2 = performance.now();
 
 console.log(`Time Elapsed : ${(t2 - t1) / 1000} seconds.`);
