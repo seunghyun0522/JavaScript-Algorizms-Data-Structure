469e75fecc46e1bc74c8a0ebbe0132e3fcf02211
seunghyun <cvtcvt007@naver.com>
2023-04-20 17:30:47 +0900

    valid Anagram

---
+++ validAnagram.js
@@ -0,0 +1,32 @@
+function validAnagram(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < a.length; i++) {
+    let letter = a[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    let letter = b[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= -1;
+    }
+  }
+
+  return true;
+}
+
+validAnagram("", ""); // true
+validAnagram("aaz", "zza"); // false
+validAnagram("anagram", "nagaram"); // true
+validAnagram("rat", "car"); // false) // false
+validAnagram("awesome", "awesom"); // false
+validAnagram("amanaplanacanalpanama", "acanalmanplanpamana"); // false
+validAnagram("qwerty", "qeywrt"); // true
+validAnagram("texttwisttime", "timetwisttext"); // true
