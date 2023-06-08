ef789334ba75006421d61abecbd7dbff8f8aee45
seunghyun0522 <cvtcvt007@naver.com>
2023-06-08 18:21:13 +0900

    Sorting_merge

--- Merge/Sorting_Merge.js
+++ Merge/Sorting_Merge.js
@@ -23,5 +23,14 @@ function merge(arr1, arr2) {
   return results;
 }
 
-const result = merge([1, 10, 50], [2, 14, 99, 100]);
-console.log(result);
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
+
+const result2 = mergeSort([10, 24, 76, 73, 72, 1, 9]);
+const result1 = merge([1, 10, 50], [2, 14, 99, 100]);
+console.log(result2);
