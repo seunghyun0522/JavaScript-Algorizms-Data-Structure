160dc98077cab60ba60edd46d05fcb860ce6278f
seunghyun0522 <cvtcvt007@naver.com>
2023-10-20 13:59:34 +0900

    pop()

--- DataStructure/SingleLinkedList/Singly-Linked_List.js
+++ DataStructure/SingleLinkedList/Singly-Linked_List.js
@@ -1,41 +1,104 @@
-// piece of data - val
-// reference to next node next
+// // piece of data - val
+// // reference to next node next
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class SinglyLinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+//   //push!
+//   push(val) {
+//     var newNode = new Node(val);
+
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//     } else {
+//       this.tail.next = newNode;
+//       this.tail = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+// }
+
+// var list = new SinglyLinkedList();
+// console.log(list);
+// list.push("A");
+// console.log(list);
+// list.push("B");
+// console.log(list);
 
 class Node {
   constructor(val) {
-    this.val = val;
+    this.value = val;
     this.next = null;
   }
 }
 
-class SinglyLinkedList {
+class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
-  //push!
-  push(val) {
-    var newNode = new Node(val);
 
+  push(value) {
+    let newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
-      this.tail = this.head;
+      this.tail = newNode;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
     }
     this.length++;
-    return this;
+  }
+  //traverse 순회
+  traverse() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+  pop() {
+    if (!this.head) return undefined;
+    else {
+      let current = this.head;
+      let newTail = current;
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+      return current;
+    }
   }
 }
 
+let l = new LinkedList();
+l.push(1);
+console.log(l);
+l.push(2);
+l.push(3);
+l.push(4);
+l.push(5);
+console.log(l);
+//l.traverse();
 
-var list = new SinglyLinkedList();
-console.log(list);
-list.push("A");
-console.log(list);
-list.push("B");
-console.log(list);
-list.push("C");
-console.log(list);
+l.pop();
+l.traverse();
+
+l.pop();
+l.traverse();
